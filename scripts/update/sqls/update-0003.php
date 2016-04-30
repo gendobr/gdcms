a7cf545e2653838dc879f4d720abe6916dad1140
dobro <dobro@gen.znu.edu.ua>
2016-04-30 21:05:23 +0300

    Merge origin/master
    Conflicts:
    	_docs/test.docx

--- scripts/update/sqls/update-0003.php
+++ scripts/update/sqls/update-0003.php
@@ -2,6 +2,18 @@
 
 return [
     'ALTER TABLE `<<tp>>site` ADD COLUMN `extra_setting` TEXT NULL AFTER `is_rssaggegator_enabled`',
-    
+    "CREATE TABLE `<<tp>>photo` (
+        `photo_id` bigint(20) NOT NULL AUTO_INCREMENT,
+        `photo_category_id` int(11) DEFAULT NULL,
+        `site_id` int(11) DEFAULT NULL,
+        `photo_visible` tinyint(1) NOT NULL DEFAULT '1',
+        `photo_title` text COLLATE utf8_bin,
+        `photo_author` text COLLATE utf8_bin,
+        `photo_description` text COLLATE utf8_bin,
+        `photo_year` int(11) DEFAULT NULL,
+        `photo_imgfile` text COLLATE utf8_bin,
+        PRIMARY KEY (`photo_id`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=252 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+      "
 
 ];
