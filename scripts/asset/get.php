78107160b94977681f72730b31f934bbf1980358
dobro <dobro@gen.znu.edu.ua>
2016-05-04 17:04:18 +0300

    Merge origin/master
    Conflicts:
    	_docs/test.docx

--- scripts/asset/get.php
+++ scripts/asset/get.php
@@ -2,26 +2,26 @@
 
 $GLOBALS['main_template_name'] = '';
 
-$rel_file_path=substr($_SERVER['REQUEST_URI'],strlen(dirname($_SERVER['PHP_SELF']))+1);
+$rel_file_path = substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['PHP_SELF'])) + 1);
 
-$filepath=realpath(\e::config('APP_ROOT').'/'.$rel_file_path);
-if($filepath && strlen($filepath)>strlen(\e::config('APP_ROOT'))){
+$filepath = realpath(\e::config('APP_ROOT') . '/' . $rel_file_path);
+if ($filepath && strlen($filepath) > strlen(\e::config('APP_ROOT'))) {
 
-	$arrayZips = array("text/css", "application/javascript",'image/png','image/jpeg','image/gif');
-    $arrayExtensions = array(".css", ".js",'.png','.jpg','.gif');
-    $extension =strtolower((false === $pos = strrpos($filepath, '.')) ? '' : substr($filepath, $pos)) ;
+    $arrayZips = array("text/css", "application/javascript", 'image/png', 'image/jpeg', 'image/gif');
+    $arrayExtensions = array(".css", ".js", '.png', '.jpg', '.gif');
+    $extension = strtolower((false === $pos = strrpos($filepath, '.')) ? '' : substr($filepath, $pos));
 
-    $pos=array_search($extension, $arrayExtensions);
-	if ($pos===false) {
+    $pos = array_search($extension, $arrayExtensions);
+    if ($pos === false) {
         $finfo = finfo_open(FILEINFO_MIME_TYPE); // возвращает mime-тип
-	    $mime_type=finfo_file($finfo, $filepath);
-	    finfo_close($finfo);
-	}else{
-		$mime_type=$arrayZips[$pos];
-	}
-	
-    header("Content-Type:".$mime_type);
-	readfile($filepath);
-}else{
-	echo "File not found";
+        $mime_type = finfo_file($finfo, $filepath);
+        finfo_close($finfo);
+    } else {
+        $mime_type = $arrayZips[$pos];
+    }
+
+    header("Content-Type:" . $mime_type);
+    readfile($filepath);
+} else {
+    echo "File $filepath not found";
 }
