10f635a0ea8dda7f61a7282486b1cbe7ff8a0f87
gendobr <gen.dobr@gmail.com>
2016-09-14 12:52:05 +0300

    +++

--- scripts/search/spider/functions.php
+++ scripts/search/spider/functions.php
@@ -123,19 +123,6 @@ function get_links($url, $html, $site_info) {
     // prn($matches[0]);
 
     $tor = Array();
-    $this_url_dirname = parse_url($url);
-    # prn($this_url_dirname);
-    if (!preg_match("/\\/\$/", $this_url_dirname['path'])) {
-        $this_url_dirname['path'] = dirname($this_url_dirname['path']) . '/';
-    }
-    $url_prefix = $this_url_dirname['scheme'] . '://' . $this_url_dirname['host'];
-    if (isset($this_url_dirname['port']))
-        $url_prefix.=':' . $this_url_dirname['port'];
-    $url_prefix.=$this_url_dirname['path'];
-
-    preg_match("/^http:\\/\\/[^\\/]+/i", $url, $host_root_url);
-    // prn($host_root_url); exit();
-    $host_root_url = $host_root_url[0];
 
     foreach ($matches[0] as $mt) {
         #prn(checkStr($mt));
@@ -152,30 +139,8 @@ function get_links($url, $html, $site_info) {
         }
         # prn($link);
         if (strlen($link) > 0) {
-            # ------------------- relative link - begin ---------------------------
-            if (!preg_match('/^(ftp|mailto|https?):/', $link)) {
-                if (preg_match("/^\\//", $link)) {
-                    $link = $host_root_url . '/' . $link;
-                } else {
-                    $link = $url_prefix . $link;
-                }
-
-                //while(ereg('/[^/]+/\.\./',$link)) {
-                while (preg_match("/\\/[^\\/]+\\/\\.\\.\\//", $link)) {
-                    $link = preg_replace('/[^\\/]+\\/\.\./', '/', $link);
-                }
-                while (preg_match("/\\/\\.\\//", $link)) {
-                    $link = preg_replace('/\./', '/', $link);
-                }
-            }
-            # ------------------- relative link - end -----------------------------
-            #prn('before $link='.$link);
-            $link = preg_replace("/#.*\$/", '', $link);
-            $link = preg_replace("/\\/+/", '/', $link);
-            $link = preg_replace("/^http:\\//", 'http://', $link);
-            #prn('after $link='.$link);
-
-            if (is_searchable($link, $site_info)) {
+	    $link = \e::url_to_absolute( $url, $link );
+	    if (is_searchable($link, $site_info)) {
                 $tor[] = $link;
             }
         }
