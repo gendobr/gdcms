82405d2a95c63d65285219fffccbb4960daeb44f
root <root@arman.gd.zp.ua>
2016-11-05 10:55:34 +0100

    +

--- scripts/ec/item/functions.php
+++ scripts/ec/item/functions.php
@@ -795,6 +795,13 @@ function ec_img_resize($inputfile, $outputfile, $width, $height, $type = "resamp
             }
             $width = $new_width;
             $height = $new_height;
+	    if($width > $size[0]){
+		$width = $size[0];
+            }
+	    if($height > $size[1]){
+		$height = $size[1];
+	    }
+	    //echo "{$size[0]}=> $width {$size[1]}=>$height";exit();
             break;
     }
     $new_left = floor(($width - $new_width) / 2);
